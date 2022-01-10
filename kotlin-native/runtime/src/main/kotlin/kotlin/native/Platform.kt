745111f02374f8b52e231c951f1496e916dc200a
Pavel Kunyavskiy <pavel.kunyavskiy@jetbrains.com>
2022-01-10 11:44:57 +0300

    fixup! [K/N] Implement Platform.availableProcessors

--- kotlin-native/runtime/src/main/kotlin/kotlin/native/Platform.kt
+++ kotlin-native/runtime/src/main/kotlin/kotlin/native/Platform.kt
@@ -104,16 +104,16 @@ public object Platform {
         set(value) = Platform_setCleanersLeakChecker(value)
 
     /**
-     * Number of logical processors available for the current thread.
+     * The number of logical processors available for the current thread.
      *
      * Can be not equal to the number of processors in the system if some restrictions on processor usage were successfully detected.
      * Some kinds of processor usage restrictions are not detected, for now, e.g., CPU quotas in containers.
      *
-     * Value will be computed on each usage. It can change if some OS scheduler API restricts the process during runtime.
+     * The value is computed on each usage. It can change if some OS scheduler API restricts the process during runtime.
      *
      * If one considers the value to be inaccurate and wants another one to be used, it can be overridden by
-     * KOTLIN_NATIVE_AVAILABLE_PROCESSORS environment variable.  When the variable is set and contains a value that is not
-     * positive Int, IllegalStateException will be thrown.
+     * [KOTLIN_NATIVE_AVAILABLE_PROCESSORS] environment variable. When the variable is set and contains a value that is not
+     * positive [Int], [IllegalStateException] will be thrown.
      */
     @ExperimentalStdlibApi
     public fun getAvailableProcessors() : Int {
@@ -122,7 +122,7 @@ public object Platform {
             return Platform_getAvailableProcessors()
         }
         return fromEnv.toIntOrNull()?.takeIf { it > 0 } ?:
-            throw IllegalStateException("Available processors has bad environment override: $fromEnv")
+            throw IllegalStateException("Available processors has incorrect environment override: $fromEnv")
     }
 }
 
